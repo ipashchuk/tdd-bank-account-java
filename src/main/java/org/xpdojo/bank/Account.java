712da06c4e9844651e9029bde0e340ea7a1503ff
Igor Pashchuk <ipashchuk@gmail.comm>
2021-01-28 10:09:22 -0500

    adding first fiew tests

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,14 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int balance;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
 }
